0ec9c0822c615f7aaaed8cb5efcf272f46a79731
Sufian Shahid <sufianthegeek@gmail.com>
2024-07-01 19:14:44 +0500

    NextJs Dashboard Project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Alkalami } from 'next/font/google';
+
+import {Lusitana} from "next/font/google"
+ 
+export const inter = Alkalami({
+    subsets: ['latin'],
+    weight: '400'
+});
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: '400'
+})
